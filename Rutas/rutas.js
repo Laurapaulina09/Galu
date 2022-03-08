be78724851d410acc1b609d92b10f3f00d3ca54f
Laurapaulina09 <laura.norenaco@amigo.edu.co>
2022-03-07 22:55:55 -0500

    Se crea el servidor y la seccion de rutas y se establece el public

---
+++ Rutas/rutas.js
@@ -0,0 +1,23 @@
+var express=require("express"),
+path = require('path'),
+router=express.Router()
+router
+    .get('/login', (req,res)=>{
+        res.sendFile(path.join(__dirname, '../vistas/login.html'))
+    })
+
+//No borrar
+/*
+    .post('/no', (req,res)=>{
+        var cuerpo = ''
+        req.on('data', data=>{
+            cuerpo = JSON.parse(data.toString())
+            console.log(cuerpo.nombre)
+        })
+        res.send('Ok')
+    })
+*/
+
+module.exports = router
+
+
