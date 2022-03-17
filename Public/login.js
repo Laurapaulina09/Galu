fb54b560b6fc5181308203502cf6fbe020b31fc3
luchodevp <lugon9999@hotmail.com>
2022-03-17 18:34:50 -0500

    Subida de archivos login.js y validacionRegistro.js

--- Public/login.js
+++ Public/login.js
@@ -1,19 +1,49 @@
 
 var form = document.getElementById('form')
+function validateEmail(email) {
+  emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+  if (emailRegex.test(email)) {
+  return true;
+  } else {
+    return false;
+  }
+};
+function validatePassword(passwd) {
+  var passwdFormat=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+  if (passwd.lenght < 7) {
+    return false;
+  }
+  if (!passwd.match(passwdFormat)) {
+    return false;
+  }
+  return true;
+}
 form.addEventListener("submit",function(event){
     event.preventDefault()
-    //lo que necesites hacer
     var correo = document.getElementById('email'),
         contrasena = document.getElementById("password")
-    if(correo.value.length > 5 && contrasena.value.length > 5){
-        alert('enviaremos tu login')           
-    }else{
-        let alerta =document.getElementById("alerta")
-        alerta.innerHTML='Tanto el correo como la contraseña deben tener mas de 5 carácteres'
+        Correo_ok=validateEmail(correo.value.trim());
+        Contrasena_ok=validatePassword(contrasena.value.trim())
+
+        if (!Correo_ok){
+          alert('introduzca un correo válido')   
+        }
+        if (!Contrasena_ok){
+          alert('Introduzca una contraseña con 8 y 15 carácteres que tenga una letra en minúscula, mayuscula y un caracter')   
+        }
+    if(Correo_ok && Contrasena_ok){
+     CreateJson(correo,contrasena)          
     }
   })
+  function CreateJson(email,passwd){ 
+    var FormSucessfull={"correo":email.value,"contraseña":passwd.value}
+   // console.log(FormSucessfull);
+    enviarFormulario('/Iniciar sesión',FormSucessfull);
+    alert("Registro Completado");
+  }
 
-  async function postData(url = '', data = {}) {
+  async function enviarFormulario(url = '', data = {}) {
     // Opciones por defecto estan marcadas con un *
     const response = await fetch(url, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
