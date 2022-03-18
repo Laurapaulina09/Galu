15c04e849dbc55d722106acb597ab21574d66d60
Gabriela <gabrielarosalesarevalo@gmail.com>
2022-03-17 19:49:09 -0500

    conexion bd

---
+++ modelo/datosb.js
@@ -0,0 +1,18 @@
+var conectar = require ("./conectdb")
+function Conexion (){
+}
+
+Conexion.almacenarUsuario= (datos,cb) => {
+    conectar.query(`INSERT INTO usuarios VALUES ("${datos.name}","${datos.email}","${datos.pass}","${datos.phone}")`, function(err, res){
+        if(err)
+        {
+            console.log(err)
+        }
+        else{
+            cb()
+
+        }
+    })
+}
+
+
